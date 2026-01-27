467fbdb1754dd39cd61dff61ab04a04f0cef2144
tucolpagos-franquicia <formulariotucolpagos@gmail.com>
2026-01-27 13:11:29 -0500

    actf

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tp2bDkxr3ejBQ1exXj1PUfzPLuqGNkfSuVFI9t2NCgg=\"\n}"
--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+    "@tailwindcss/postcss": {},
+    autoprefixer: {}
+  }
+};
 
 
 
